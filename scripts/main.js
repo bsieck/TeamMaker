ada4d32e5ea99a6002dad491d27ff5b9507bc2a1
Brennan Sieck <bsieck@umich.edu>
2019-11-29 11:38:20 -0500

    end of jake's work

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,60 @@ $(window).on("load", function() {
         })
         return app.numPlayers() + 1;
     }
+    var teams = new Vue({
+        el: "#teamList",
+        data: {
+            numTeams: 2,
+            teams: [[],[]]
+        },
+        methods: {
+            "updateNumTeams": function() {
+                teams.numTeams = 2;
+            },
+            "makeTeams": function() {
+                // verify player list
+                app.players = app.players.filter(d => d.name !== "" & d.name !== "Enter a name...")
+                teams.teams = [[],[]]
+                var playersByElo = JSON.parse(JSON.stringify(app.players))
+                playersByElo.sort(function(a, b) {
+                    return b.elo - a.elo;
+                });
+                playersByElo.map(function(d, i) {
+                    teamIndex = i % teams.numTeams
+                    teams.teams[teamIndex].push(d)
+                })
+                console.log(teams.teams)
+                // // more complex version
+                // var ranks = [];
+                // var curr_rank = 1;
+                // var curr_elo = -1;
+                // for (var i in playersByElo) {
+                //     if (curr_elo !== playersByElo[i].elo) {
+                //         ranks.push(curr_rank)
+                //         curr_rank += 1
+                //     } else {
+                //         ranks.push(curr_rank)
+                //     }
+                //     curr_elo = playersByElo[i].elo
+                // }
+                // console.log(ranks)
+                // //rank: id
+                // //then grab odd/even ranks
+                // var teamAids = []
+                // ranks.map(function(){})
+                // console.log(playersByElo)
+                // var rankedPlayers = {};
+                // playersByElo.map(function(d, i){
+                //     var rank = i
+                //     var curr_elo = d.elo
+                //     var num_equal_players = playersByElo.filter(d => d.elo === curr_elo)
+                    
+                //     return ;
+                // })
+
+            }
+        }
+    })
     var app = new Vue({
         el: '#playerList',
         data: {
@@ -57,47 +111,11 @@ $(window).on("load", function() {
             "numPlayers": function() {
                 if (app !== undefined){
                     var len = app.players.length
-                    console.log(len)
                     return len;
                 }
             },
             "clearName": function(id) {
-                console.log(id)
                 app.players.filter(d => d.id === id)[0].name=""
-                console.log("id")
-            },
-            "makeTeams": function() {
-                console.log(app.players)
-                var playersByElo = app.players.sort(function(a, b) {
-                    return a.elo - b.elo;
-                });
-                var ranks = [];
-                var curr_rank = 1;
-                var curr_elo = -1;
-                for (var i in playersByElo) {
-                    if (curr_elo !== playersByElo[i].elo) {
-                        ranks.push(curr_rank)
-                        curr_rank += 1
-                    } else {
-                        ranks.push(curr_rank)
-                    }
-                    curr_elo = playersByElo[i].elo
-                }
-                console.log(ranks)
-                //rank: id
-                //then grab odd/even ranks
-                var teamAids = []
-                ranks.map(function)
-                console.log(playersByElo)
-                var rankedPlayers = {};
-                playersByElo.map(function(d, i){
-                    var rank = i
-                    var curr_elo = d.elo
-                    var num_equal_players = playersByElo.filter(d => d.elo === curr_elo)
-                    
-                    return ;
-                })
-
             }
 
         }
